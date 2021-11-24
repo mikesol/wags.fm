ec766c471e0ae33c18aa29a5beda19d7430a3ea2
Mike Solomon <mike@wavr.so>
2021-11-24 12:56:22 +0200

    Adds animation ahck

---
+++ src/CSS/Hack/Animation.purs
@@ -0,0 +1,91 @@
+module CSS.Hack.Animation where
+
+import Prelude
+
+import CSS.Property (class Val, Value, value)
+import CSS.String (class IsString, fromString)
+import CSS.Stylesheet (CSS, key)
+import CSS.Time (Time)
+import CSS.Transition (TimingFunction)
+import Data.Foldable (for_)
+import Data.Tuple.Nested (tuple8)
+
+newtype AnimationDirection = AnimationDirection Value
+
+derive instance eqAnimationDirection :: Eq AnimationDirection
+derive instance ordAnimationDirection :: Ord AnimationDirection
+
+instance valAnimationDirection :: Val AnimationDirection where
+  value (AnimationDirection v) = v
+
+normalAnimationDirection :: AnimationDirection
+normalAnimationDirection = AnimationDirection $ fromString "normal"
+
+alternate :: AnimationDirection
+alternate = AnimationDirection $ fromString "alternate"
+
+reverse :: AnimationDirection
+reverse = AnimationDirection $ fromString "reverse"
+
+alternateReverse :: AnimationDirection
+alternateReverse = AnimationDirection $ fromString "alternate-reverse"
+
+newtype IterationCount = IterationCount Value
+
+derive instance eqIterationCount :: Eq IterationCount
+derive instance ordIterationCount :: Ord IterationCount
+
+instance valIterationCount :: Val IterationCount where
+  value (IterationCount v) = v
+
+infinite :: IterationCount
+infinite = IterationCount $ fromString "infinite"
+
+iterationCount :: Number -> IterationCount
+iterationCount = IterationCount <<< value
+
+newtype FillMode = FillMode Value
+
+derive instance eqFillMode :: Eq FillMode
+derive instance ordFillMode :: Ord FillMode
+
+instance valFillMode :: Val FillMode where
+  value (FillMode v) = v
+
+forwards :: FillMode
+forwards = FillMode $ fromString "forwards"
+
+backwards :: FillMode
+backwards = FillMode $ fromString "backwards"
+
+data AnimationPlayState = APaused | ARunning | AInitial
+
+instance valAnimationState :: Val AnimationPlayState where
+  value = fromString <<< case _ of
+    APaused -> "paused"
+    ARunning -> "running"
+    AInitial -> "initial"
+
+
+animation :: AnimationName -> Time -> TimingFunction -> Time -> IterationCount -> AnimationDirection -> FillMode -> AnimationPlayState -> CSS
+animation p de f du i di fm ps = do
+  for_ animationKeys \k ->
+    key (fromString k) (tuple8 p de f du i di fm ps)
+  where
+  animationKeys =
+    [ "animation"
+    , "-webkit-animation"
+    , "-moz-animation"
+    , "-o-animation"
+    ]
+
+newtype AnimationName = AnimationName Value
+
+derive instance eqAnimationName :: Eq AnimationName
+derive instance ordAnimationName :: Ord AnimationName
+
+instance valAnimationName :: Val AnimationName where
+  value (AnimationName v) = v
+
+instance isStringAnimationName :: IsString AnimationName where
+  fromString = AnimationName <<< fromString
