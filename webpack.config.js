7f3a5374fe7f81a3e8a2b21b0692eca778d00d9b
Mike Solomon <mike@wavr.so>
2021-11-24 23:57:18 +0200

    Reduces bundle slightly

--- webpack.config.js
+++ webpack.config.js
@@ -2,56 +2,64 @@ var path = require("path");
 var webpack = require("webpack");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-	mode: "development",
-	entry: process.env.WEBPACK_ENTRY
-		? process.env.WEBPACK_ENTRY
-		: "./src/index.js",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "bundle.js",
-		clean: true,
-	},
-	plugins: [
-		new HtmlWebpackPlugin({template:"index.html"}),
-	],
-	resolve: {
-		alias: {
-			"../Halogen.Component/index.js": "../../src/Halogen.Component.patch.js",
+module.exports = (env) => {
+	var base = {
+		mode: env.production ? "production" : "development",
+		entry: process.env.WEBPACK_ENTRY
+			? process.env.WEBPACK_ENTRY
+			: "./src/index.js",
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: "bundle.js",
+			clean: true,
 		},
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/i,
-				include: path.resolve(__dirname, "src"),
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["@babel/preset-env"],
+		plugins: [new HtmlWebpackPlugin({ template: "index.html" })],
+		module: {
+			rules: [
+				{
+					test: /\.js$/i,
+					include: path.resolve(__dirname, "src"),
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
+						},
 					},
 				},
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: "asset/resource",
-			},
-			{
-				test: /\.purs$/i,
-				include: path.resolve(__dirname, "src"),
-				type: "asset/source",
-			},
-			{
-				test: /\.css$/i,
-				include: path.resolve(__dirname, "src"),
-				use: ["style-loader", "css-loader", "postcss-loader"],
-			},
-		],
-	},
-	devServer: {
-		contentBase: path.resolve(__dirname, "dist"),
-		watchContentBase: true,
-		disableHostCheck: true,
-		hot: true,
-	},
+				{
+					test: /\.(png|svg|jpg|jpeg|gif)$/i,
+					type: "asset/resource",
+				},
+				{
+					test: /\.purs$/i,
+					include: path.resolve(__dirname, "src"),
+					type: "asset/source",
+				},
+				{
+					test: /\.css$/i,
+					include: path.resolve(__dirname, "src"),
+					use: ["style-loader", "css-loader", "postcss-loader"],
+				},
+			],
+		},
+		devServer: {
+			contentBase: path.resolve(__dirname, "dist"),
+			watchContentBase: true,
+			disableHostCheck: true,
+			hot: true,
+		},
+	};
+	return Object.assign(
+		env.production
+			? {}
+			: {
+					resolve: {
+						alias: {
+							"../Halogen.Component/index.js":
+								"../../src/Halogen.Component.patch.js",
+						},
+					},
+			  },
+		base
+	);
 };
