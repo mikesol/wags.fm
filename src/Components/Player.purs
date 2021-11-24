6b6e246ae402ec280defbc55dc034ccda684e735
Mike Solomon <mike@wavr.so>
2021-11-24 15:51:05 +0200

    Slightly shorter delay

--- src/Components/Player.purs
+++ src/Components/Player.purs
@@ -214,7 +214,7 @@ component =
   handleAction = match
     { pressPlay: const $ do
         H.raise pressPlay
-        H.liftAff $ delay (Milliseconds 750.0)
+        H.liftAff $ delay (Milliseconds 400.0)
         { isPlaying, hiddenInstr: { hidden } } <- H.get
         when (isPlaying && not hidden) $ H.raise $ hidePlayer { transitionTime: 0.995 }
     , pressStop: const $ do
